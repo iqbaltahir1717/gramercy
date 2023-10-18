39c29a1f0804a13171680c4a16503628ccfcf8ee
pausia <kenojiaa@gmail.com>
2023-10-18 22:29:52 +0700

    Perubahan halaman home page

--- application/views/landing_page/index.php
+++ application/views/landing_page/index.php
@@ -93,8 +93,14 @@
             <div class="row lokasi justify-content-center text-center">
                 <p class="mt-5">Located in Alam Sutera, South Tangerang, The Gramercy is an oasis from the bustling urban lifestyle. A well-built residential development, Alam Sutera offers a better quality of life, amidst mature greenery, complete sports facilities, office complexes, entertainment, and other support infrastructure like schools and a hospital. The Gramercy provides all your needs. A place to express gratitude and begin your story.</p>
             </div>
-            <div class="row lokasi justify-content-center text-center mt-5">
-                <p class="mt-5">The last and most prestigious cluster locatedin favourite place of Alam Sutera “Green Tunnel”</p>
+            <div class="row lokasi justify-content-center text-center mt-3">
+                <button type="button" class="btn btn-transparent btn-sm-">Location <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="row lokasi justify-content-center text-center mt-5 mb-5">
+                <img src="assets/core-images/location.png" alt="Image 2">
             </div>
         </div>
     </section>
