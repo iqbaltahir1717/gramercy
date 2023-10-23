b9fc96e17b2d0d468c93b3388d5367c59ae10c2e
pausia <kenojiaa@gmail.com>
2023-10-23 13:25:14 +0700

    Revisi Navbar dan Hero Home

--- application/views/landing_page/index.php
+++ application/views/landing_page/index.php
@@ -2,9 +2,9 @@
 <script src="<?= base_url('vendor/twbs/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
     <main id="main mt-5">
     <section id="hero-new" class="hero-new mt-2">
-        <div class="container barisone md-12">
-            <div class="row align-items-center">
-                <div class="col-md-10">
+        <div class="container-md-12 pl-5 pr-5 barisone md-12">
+            <div class="row pl-5 pr-5 align-items-start">
+                <div class="col-md-12">
                     <h1 class="bannertext">ELEVATE YOUR LIFESTYLE</h1>
                 </div>
                 <!-- <div class="bulatan col-md-2 d-flex justify-content-end">
@@ -21,19 +21,19 @@
                 </div> -->
             </div>
         </div>
-        <div class="container baristwo md-12">
+        <div class="container-md-12 pr-5 baristwo md-12">
             <div class="row align-items-start">
                 <div class="col-md-6">
                     <h1 class="bannertext">LUXURY HOMES</h1>
                 </div>
-                <div class="col-md-6 d-flex">
-                    <p class="my-text">
+                <div class="col-md-6 d-flex justify-content-end">
+                    <p class="my-text text-center">
                         A gift of gratitude, Alam Sutera offers a sanctuary for serene living in thanks for the achievements attained. Not just a house, The Gramercy is a home where you can express yourself, appreciate life, connect with others, and create life memories.
                     </p>
                 </div>
             </div>
         </div>
-        <div class="container baristree md-12">
+        <div class="container-md-12 pr-5 baristree md-12">
             <div class="row align-items-center">
                 <div class="col-md-12 justify-content-end">
                     <h1 class="bannertext">EXCLUSIVE  DREAMS</h1>
@@ -42,7 +42,7 @@
         </div>
     </section>
     <!-- --------------------------Section 2----------------------------------- -->
-    <section id="hero-new2" class="hero-new2">
+    <section id="hero-new2" class="hero-new2 mt-2">
             <div class="container">
                 <div class="row">
                     <div class="image-container">
@@ -52,8 +52,8 @@
     </section>
 
     <!-- --------------------------Section 3----------------------------------- -->
-    <section id="hero-new3" class="hero-new3 mt-0">
-        <div class="container section3 justify-content-center text-center">
+    <section id="hero-new3" class="hero-new3">
+        <div class="container-md-12 pl-5 pr-5 barisone md-12">
             <div class="row brand-logo-gramercy justify-content-center text-center">
                 <div class="col">
                     <img src="assets/core-images/alam-sutera.png" alt="Image 1">
@@ -62,20 +62,24 @@
                     <img src="assets/core-images/thegramercy.png" alt="Image 2">
                 </div>
             </div>
+        </div>
+        <div class="container  pl-5 pr-5 barisone md-12">
             <div class="row ourservice justify-content-center text-center">
-                <div class="col md-1">
-                    <h3>GRATITUDE</h3>
-                    <h5>Alam Sutere gives back to it’s  greater community by developing a sanctuary for serene living. A surprise gift for gratitude</h5>
+                    <div class="col-md-4 bagian">
+                        <h3>GRATITUDE</h3>
+                        <h5>Alam Sutere gives back to it’s  greater community by developing a sanctuary for serene living. A surprise gift for gratitude</h5>
+                    </div>
+                    <div class="col-md-4 bagian">
+                        <h3>FELLOWSHIP</h3>
+                        <h5>Build Opennes of heart with others who share common interest to learn. Grow and enjoy life</h5>
+                    </div>
+                    <div class="col-md-4 bagian">
+                        <h3>FREEDOM</h3>
+                        <h5>Creation of curated gratitude community and well designed environment</h5>
+                    </div>
                 </div>
-                <div class="col md-1">
-                    <h3>FELLOWSHIP</h3>
-                    <h5>Build Opennes of heart with others who share common interest to learn. Grow and enjoy life</h5>
-                </div>
-                <div class="col md-1">
-                    <h3>FREEDOM</h3>
-                    <h5>Creation of curated gratitude community and well designed environment</h5>
-                </div>
-            </div>
+        </div>
+        <div class="container">
             <div class="row ourservice justify-content-center text-center mt-5">
                 <p class="mt-5">The last and most prestigious cluster locatedin favourite place of Alam Sutera “Green Tunnel”</p>
             </div>
@@ -108,14 +112,14 @@
     <!-- --------------------------Section 5----------------------------------- -->
 
     <section id="hero-new5" class="hero-new5 mt-5">
-        <div class="container barisone md-12">
+        <div class="container-md-12 barisone">
             <div class="row align-items-end">
-                <div class="col-md-7">
-                    <h1 class="bannertext">FIND YOUR <br>LUXURY UNIT</h1>
+                <div class="col-md-7 pr-5 pl-5">
+                    <h1 class="bannertext pl-5">FIND YOUR <br>LUXURY UNIT</h1>
                 </div>
-                <div class="prenext col-md-5 d-flex justify-content-end align-items-end">
-                    <div class="col">
-                        <ul class="pagination pagination-sm">
+                <div class="prenext col-md-5 pr-5 d-flex justify-content-end align-items-end  text-right">
+                    <div class="col-md-11 justify-content-end align-items-end  text-right">
+                        <ul class="pagination pagination-sm justify-content-end align-items-end  text-right">
                             <li class="page-item tombol-left d-flex"><a class="page-link  transparent-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
                                 </svg>                                
@@ -129,8 +133,8 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="col">
-                        <h2 class="bannertext">1/4</h2>
+                    <div class="col-md-0 pr-5 justify-content-end align-items-end  text-right">
+                        <h2 class="bannertext justify-content-end align-items-end  text-right">1/4</h2>
                     </div>
 
                 </div>
@@ -141,18 +145,18 @@
     <!-- --------------------------Section 6----------------------------------- -->
 
     <section id="hero-new6" class="hero-new6">
-        <div class="container md-12">
+        <div class="container-md-12 pl-5 pr-5">
             <div class="row align-items-center">
                 <div class="col-md-6 align-items-start">
-                    <div class="row align-items-center">
-                        <div class="col-md-5 align-items-start">
+                    <div class="row align-items-start">
+                        <div class="col-md-5 pl-5 justify-content-start align-items-start  text-left">
                             <img class="brand" src="assets/core-images/thegramercyhitam.png" alt="Image 2">
                         </div>
-                        <div class="col-md-7 align-items-start">
+                        <div class="col-md-7 justify-content-start align-items-start  text-left">
                             <h1>UNIT ALCA</h1>
                         </div>
                     </div>
-                    <div class="row mt-5">
+                    <div class="row mt-5 pl-5">
                         <p>
                             Lorem ipsum dolor sit amet consectetur. Feugiat pellentesque aenean id ut massa dui proin ultrices. Id pharetra lorem ut id urna. Tempus metus ullamcorper enim purus libero amet ornare in. Tortor enim sed aliquam purus elementum purus commodo sit faucibus. Phasellus euismod porttitor sit enim vitae porta magna. Sagittis varius accumsan vulputate vulputate volutpat pulvinar integer pellentesque elit. Ultricies tellus arcu nunc leo dolor nullam risus ut. Dis integer vulputate pulvinar turpis vulputate duis mauris velit ac. Dictum scelerisque aliquet ac risus lacinia placerat sapien blandit. Proin sit non id duis scelerisque dui enim nisl gravida. Senectus est lacinia egestas sed cursus dolor. Aenean sed nisl quis amet cursus. Dictum mi sit id eleifend interdum in. Semper eu fringilla penatibus gravida.
                         </p>
@@ -160,24 +164,24 @@
                             Ultricies tellus arcu nunc leo dolor nullam risus ut. Dis integer vulputate pulvinar turpis vulputate duis mauris velit ac. Dictum scelerisque aliquet ac risus lacinia placerat sapien blandit.
                         </p>
                     </div>
-                    <div class="row mt-2">
+                    <div class="row mt-2 pl-5">
                         <button type="button justify-align-start" class="btn btn-transparent btn-sm-">Location <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                             </svg>
                         </button>
                     </div>
                 </div>
-                <div class="col-md-6 justify-align-center">
-                    <img class="unitalca" src="assets/core-images/unitalca.svg" alt="Image 2">
+                <div class="col-md-6 pr-5 justify-content-end align-items-end  text-right">
+                    <img class="unitalca justify-content-end align-items-end  text-right" src="assets/core-images/unitalca.svg" alt="Image 2">
                 </div>
             </div>
         </div>
     </section>
     <!-- --------------------------Section 7----------------------------------- -->
     <section id="hero-new7" class="hero-new7">
-            <div class="container md-12 align-items-center">
-                <div class="row align-items-start justify-align-center">
-                    <div class="col md-11">
+            <div class="container-md-12 pl-5 align-items-start">
+                <div class="row pl-5 align-items-start justify-align-start">
+                    <div class="col-md-12">
                         <div class="row mt-5">
                             <img class="brand" src="assets/core-images/thegramercyputih.png" alt="Image 2">
                         </div>
@@ -185,7 +189,7 @@
                             <h1>LUSH COURTYARD SANCTUARY</h1>
                         </div>
                         <div class="row mt-5">
-                            <div class="col">
+                            <div class="col pl-2">
                                 <div class="card">
                                 <div class="card-body">
                                     <p>A home where you want to be. Surround yourself with lush greenery that captivates your life’s gratitude. Brought to life by Karl Princic, the landscape architect with experience over 30 years in planning and designing of resorts, residential and hospitality projects worldwide. Now the paradise is yours.</p>
@@ -197,9 +201,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col md-1">
-
                     </div>
                 </div>
             </div>
